33f87a90e94d4fc7e7c0723cba99cf2754c06069
Zapel <zapel176@ukr.net>
2018-04-09 08:30:50 +0300

    lesson49
    hash

--- src/main/java/Hash/Main.java
+++ src/main/java/Hash/Main.java
@@ -1,16 +1,60 @@
 package Hash;
 
-public class Main extends Object{
-    int value;
-    public static void main(String[] args) {
-        Main main =new Main();
-        main.value = 5;
-        System.out.println(main.toString());
-        System.out.println(main + "");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+class Book {
+    String author;
+    String name;
+    String format;
+}
+
+class Ticket{
+    int number;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return number == ticket.number;
     }
 
     @Override
-    public String toString(){
-        return "value" + value;
+    public int hashCode() {
+        return number;
+    }
+}
+
+public class Main extends Object{
+
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+        ticket.number = 1123;
+
+        Book book = new Book();
+        book.author = "Dale Carnegie";
+        book.name = "How to get friends";
+        book.format = "Big";
+
+        Map<Ticket, Book> library = new HashMap<Ticket, Book>();
+        library.put(ticket, book);
+
+        Ticket ticket2 = new Ticket();
+        ticket2.number = 1123;
+
+        //Book daleBook = library.get(ticket);
+        //System.out.println(daleBook.author);
+        //System.out.println(daleBook.name);
+        //System.out.println(daleBook.format);
+
+        Book daleBook = library.get(ticket2);
+        System.out.println(daleBook.author);
+        System.out.println(daleBook.name);
+        System.out.println(daleBook.format);
+
+        //System.out.println(book.hashCode());
+        //System.out.println(new Book().hashCode());
     }
 }
