857fa763a3a685f28bcfa2ad7e7dbd001752d5b3
Zapel <34133527+Zapel@users.noreply.github.com>
2018-04-20 17:32:49 +0300

    lesson70
    thread

---
+++ src/main/java/Thread/lesson70/VolatileMain.java
@@ -0,0 +1,16 @@
+package Thread.lesson70;
+
+public class VolatileMain {
+    int i;
+
+    public static void main(String[] args) {
+
+    }
+
+    static class MtThreadWrite extends Thread{
+
+    }
+
+}
+
+
