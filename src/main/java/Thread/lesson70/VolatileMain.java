069d04d63b87c6664f885a566dc39b8310e551ff
Zapel <34133527+Zapel@users.noreply.github.com>
2018-04-23 10:45:55 +0300

    lesson70
    volatation

--- src/main/java/Thread/lesson70/VolatileMain.java
+++ src/main/java/Thread/lesson70/VolatileMain.java
@@ -1,16 +1,40 @@
 package Thread.lesson70;
 
 public class VolatileMain {
-    int i;
+    volatile static int i;
 
     public static void main(String[] args) {
+        new MyThreadRead().start();
+        new MyThreadWrite().start();
 
     }
 
-    static class MtThreadWrite extends Thread{
-
+    static class MyThreadWrite extends Thread {
+        @Override
+        public void run() {
+            while (i < 5) {
+                System.out.println("increment i to " + (++ i));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
+    static class MyThreadRead extends Thread {
+        @Override
+        public void run() {
+            int localVar = i;
+            while (localVar < 5) {
+                if(localVar != i) {
+                    System.out.println("new value of i is " + i);
+                    localVar = i;
+                }
+            }
+        }
+    }
 }
 
 
