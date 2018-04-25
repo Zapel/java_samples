706716222b85e383cc8aae4630fa389d0116c0c0
Zapel <34133527+Zapel@users.noreply.github.com>
2018-04-25 14:51:12 +0300

    lesson85
    CyclicBarrier

---
+++ src/main/java/Thread/lesson85/Main.java
@@ -0,0 +1,40 @@
+package Thread.lesson85;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Main {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new Run());
+        new Sportsman(cyclicBarrier);
+        new Sportsman(cyclicBarrier);
+        new Sportsman(cyclicBarrier);
+    }
+
+    static class Run  extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Run is began.");
+        }
+    }
+
+    static class Sportsman extends Thread {
+        CyclicBarrier barrier;
+
+        public Sportsman(CyclicBarrier barrier) {
+            this.barrier = barrier;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
