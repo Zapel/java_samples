6728ddbf6bc1a67095018ac56ffce0b0c0be92cc
Zapel <34133527+Zapel@users.noreply.github.com>
2018-04-25 16:11:21 +0300

    lesson88

---
+++ src/main/java/Thread/lesson87/Main.java
@@ -0,0 +1,33 @@
+package Thread.lesson87;
+
+import sun.security.provider.NativePRNG;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new PriorityBlockingQueue<>();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                queue.add("this is string");
+            }
+        }.start();
+
+    }
+}
