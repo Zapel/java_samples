806fd6b49303f192ae4b69add0246780cd151daa
Zapel <34133527+Zapel@users.noreply.github.com>
2018-05-23 17:40:28 +0300

    lesson179

---
+++ src/main/java/TCP_IP/Main2.java
@@ -0,0 +1,18 @@
+package TCP_IP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        try(ServerSocket serverSocket = new ServerSocket(8189)) {
+            Socket socket = serverSocket.accept();
+            Scanner scanner = new Scanner(socket.getInputStream());
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("Hello");
+        }
+    }
+}
