a89936aaa5da0640320ef18a94051ec915d05de3
Zapel <zapel176@ukr.net>
2018-05-25 20:57:15 +0300

    test

--- src/main/java/TCP_IP/EmailLessons.java
+++ src/main/java/TCP_IP/EmailLessons.java
@@ -1,6 +1,9 @@
 package TCP_IP;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -8,13 +11,19 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class EmailLessons {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, MessagingException {
         final Properties properties = new Properties();
         properties.load(EmailLessons.class.getClassLoader().getResourceAsStream("mail.properties"));
 
         Session mailSession = Session.getDefaultInstance(properties);
         MimeMessage message = new MimeMessage(mailSession);
-        message.setFrom(new InternetAddress());
+        message.setFrom(new InternetAddress("lazebaoleg"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress("zapel176@ukr.net"));
+        message.setSubject("hello");
+        message.setText("Hi this is my test message");
+
+        
+
 
 
     }
