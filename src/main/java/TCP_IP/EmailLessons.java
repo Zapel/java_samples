7460e146d35022413f8e5073649d853f1b7a721f
Zapel <zapel176@ukr.net>
2018-06-11 07:24:48 +0300

    home

--- src/main/java/TCP_IP/EmailLessons.java
+++ src/main/java/TCP_IP/EmailLessons.java
@@ -6,24 +6,51 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.*;
+import java.io.Console;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
 
 public class EmailLessons {
     public static void main(String[] args) throws IOException, MessagingException {
-        final Properties properties = new Properties();
-        properties.load(EmailLessons.class.getClassLoader().getResourceAsStream("email.properties"));
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream ( Paths.get("mail","mail.properties"))) {
+            props.load(in);
+        }
+        List<String> lines = Files.readAllLines (Paths.get(args[0]), Charset.forName ("UTF-8"));
 
-        Session mailSession = Session.getDefaultInstance(properties);
+        String from = lines.get (0);
+        String to = lines.get (1);
+        String subject = lines.get (2);
+
+        StringBuilder builder = new StringBuilder ();
+        for (int i = 3; i < lines.size (); i ++) {
+            builder.append (lines.get (i));
+            builder.append ("\n");
+        }
+
+        Console console = System.console ();
+        String password = new String (console.readPassword ("Password: "));
+
+        Session mailSession = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(mailSession);
-        message.setFrom(new InternetAddress("oleg.lazeba@chint-ukraine.com"));
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress("zapel176@ukr.net"));
-        message.setSubject("hello");
-        message.setText("Hi this is my test message");
+        message.setFrom(new InternetAddress(from));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.setSubject(subject);
+        message.setText(builder.toString ());
 
         Transport tr = mailSession.getTransport();
-        tr.connect(null, "123456!a");
-        tr.sendMessage(message, message.getAllRecipients());
-        tr.close();
+        try {
+            tr.connect(null, password);
+            tr.sendMessage(message, message.getAllRecipients());
+        } finally {
+            tr.close();
+        }
     }
 }
