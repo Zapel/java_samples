536912d4b02c383ba556b8b27d372cb3f3413bb6
Zapel <zapel176@ukr.net>
2018-07-21 11:04:23 +0300

    lesson284

---
+++ src/main/java/Patterns/ChainOfResponsibility.java
@@ -0,0 +1,48 @@
+package Patterns;
+
+public class ChainOfResponsibility {
+    public static void main(String[] args) {
+        MessagePrinter messagePrinter = new ConsoleMessagePrinter ();
+        FileMessagePrinter fileMessagePrinter = new FileMessagePrinter ();
+        messagePrinter.setNextMessagePrinter (fileMessagePrinter);
+        fileMessagePrinter.setNextMessagePrinter ( new DbMessagePrinter () );
+        messagePrinter.print ( "hello" );
+    }
+}
+
+abstract class MessagePrinter {
+    MessagePrinter nextMessagePrinter;
+
+    void setNextMessagePrinter(MessagePrinter messagePrinter) {
+        nextMessagePrinter = messagePrinter;
+    }
+
+    void print(String message) {
+        printMessage ( message );
+        if(nextMessagePrinter != null) {
+            nextMessagePrinter.print ( message );
+        }
+    }
+    abstract void printMessage(String message);
+}
+
+class ConsoleMessagePrinter extends MessagePrinter {
+    @Override
+    void printMessage(String message) {
+        System.out.println ("print to console: " + message);
+    }
+}
+
+class FileMessagePrinter extends MessagePrinter {
+    @Override
+    void printMessage(String message) {
+        System.out.println ("print to file: " + message);
+    }
+}
+
+class DbMessagePrinter extends MessagePrinter {
+    @Override
+    void printMessage(String message) {
+        System.out.println ("print to Db: " + message);
+    }
+}
